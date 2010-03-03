b6292eba6e8ab30b8620de1d8a6436107f8a69c8
kurilo <kurilo@1e4866f0-dd02-11de-b4e9-91c67fb6d0f2>
2010-03-03 18:16:01 +0000

    added logging header

---
+++ src/logging.h
@@ -0,0 +1,8 @@
+/**
+ * \file
+ *         Logging facilites for the SNMP server
+ * \author
+ *         Siarhei Kuryla <kurilo@gmail.com>
+ *
+ * @(#)$Id: mtarch.h,v 1.2 2006/12/22 16:55:53 barner Exp $
+ */
