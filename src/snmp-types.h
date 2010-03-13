63f59dcf11c41a08ca8c7c842839ccea9b59b527
kurilo <kurilo@1e4866f0-dd02-11de-b4e9-91c67fb6d0f2>
2010-03-13 17:04:36 +0000

    added types

---
+++ src/snmp-types.h
@@ -0,0 +1,41 @@
+/* -----------------------------------------------------------------------------
+ * SNMP implementation for Contiki
+ *
+ * Copyright (C) 2010 Siarhei Kuryla <kurilo@gmail.com>
+ *
+ * This program is part of free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+/**
+ * \file
+ *         Types used in the SNMP protocol implementation
+ * \author
+ *         Siarhei Kuryla <kurilo@gmail.com>
+ */
+
+#ifndef __SNMPD_TYPES_H__
+#define __SNMPD_TYPES_H__
+
+/** \brief indicates whether debug is enabled */
+#define u8_t unsigned char
+#define s8_t signed char
+#define u16_t unsigned short
+#define s16_t signed short
+#define u32_t unsigned long
+#define s32_t signed long
+
+
+#endif /* __SNMPD_TYPES_H__ */
