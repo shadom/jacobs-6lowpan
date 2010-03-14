54c4f416e974eff745cb7bbf07b27662d910cd8a
kurilo <kurilo@1e4866f0-dd02-11de-b4e9-91c67fb6d0f2>
2010-03-14 23:21:41 +0000

    commit before the refactoring ber functionality

---
+++ src/ber.h
@@ -0,0 +1,37 @@
+/* -----------------------------------------------------------------------------
+ * SNMP implementation for Contiki
+ *
+ * Copyright (C) 2010 Siarhei Kuryla
+ *
+ * This program is part of free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+/**
+ * \file
+ *         BER encoding and decoding
+ * \author
+ *         Siarhei Kuryla <kurilo@gmail.com>
+ */
+
+#ifndef __BER_H__
+#define	__BER_H__
+
+
+
+
+
+#endif	/* __BER_H__ */
+
---
+++ src/mib.h
@@ -0,0 +1,30 @@
+/* -----------------------------------------------------------------------------
+ * SNMP implementation for Contiki
+ *
+ * Copyright (C) 2010 Siarhei Kuryla <kurilo@gmail.com>
+ *
+ * This program is part of free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#ifndef __MIB_H__
+#define __MIB_H__
+
+#include "snmpd-types.h"
+
+//static const oid_t m_system_oid		= { { 1, 3, 6, 1, 2, 1, 1			}, 7, 8  };
+
+#endif /* __MIB_H__ */
+
